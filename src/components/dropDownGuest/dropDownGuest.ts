ee7326047f48bef4f801100a4174fa5b91fe64da
“coder1” <c.o.d.e.r.1@mail.ru>
2021-06-08 21:00:06 +0400

    wrote an implementation on TypeScript DropDown - 40%

--- src/components/dropDownGuest/dropDownGuest.ts
+++ src/components/dropDownGuest/dropDownGuest.ts
@@ -1,11 +1,77 @@
-
-// class dropDownGuest {
-// 	constructor() {
-
-// 	}
+import $ from "jquery";
 
 
-// }
+class dropDownGuest {
+
+	adults: Element;
+	children: Element;
+	babies: Element;
+	inputEl: Element;
+	imgEl: Element;
+	selectEl: Element;
+
+	// eslint-disable-next-line no-unused-vars
+	constructor(public className: string) {
+		this.setDomElem();
+		this.setActionInput();
+
+	}
+
+	setDomElem() {
+		const dropClass = '.' + this.className;
+		const inputClass = dropClass + '__input';
+		const imgClass = dropClass + '__tip';
+		const selectClass = dropClass + '__select';
+
+		const elem = document.querySelector('.' + this.className);
+
+		this.adults = elem.querySelector('[data-type="adults"]');
+		this.children = elem.querySelector('[data-type="children"]');
+		this.babies = elem.querySelector('[data-type="babies"]');
+
+		this.inputEl = elem.querySelector(inputClass);
+		this.imgEl = elem.querySelector(imgClass);
+		this.selectEl = elem.querySelector(selectClass);
+	}
+
+
+
+	setActionInput() {
+		let flag = false;
+		let funAct = () => {
+
+			if (!flag) {
+				$(this.inputEl).css({
+					"border-bottom-left-radius": "0px",
+					"border-bottom-right-radius": "0px",
+					"border": "1px solid rgba(31, 32, 65, 0.5)"
+				});
+				flag = true;
+			}
+			else {
+				$(this.inputEl).css({
+					"border-bottom-left-radius": "4px",
+					"border-bottom-right-radius": "4px",
+					"border": "1px solid rgba(31, 32, 65, 0.25)"
+				});
+				flag = false;
+			}
+
+			$(this.selectEl).toggle();
+		};
+
+		$(this.inputEl).on("click", funAct);
+		$(this.imgEl).on("click", funAct);
+	}
+
+
+
+}
+
+
+
+
+new dropDownGuest('dropDownGuest');
 
 
 // клик по инпуту - показать селектор или скрыть (изменить стили)
