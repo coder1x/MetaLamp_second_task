693e52e31bc7a7cc8d573f529c804aec697ef6bd
“coder1” <c.o.d.e.r.1@mail.ru>
2021-06-10 21:56:38 +0400

    ready dropDownGuest

--- src/components/dropDownGuest/dropDownGuest.ts
+++ src/components/dropDownGuest/dropDownGuest.ts
@@ -3,65 +3,189 @@ import $ from "jquery";
 
 class dropDownGuest {
 
-	adults: Element;
-	children: Element;
-	babies: Element;
-	inputEl: Element;
-	imgEl: Element;
+	inputEl: any;
+	imgClass: string;
 	selectEl: Element;
+	$clearBut: any;
+	applyClass: string;
+	elem: Element;
+	items: any;
+	defoultText: string;
+	adultsEl: any;
+	childrenEl: any;
+	babiesEl: any;
+
 
 	// eslint-disable-next-line no-unused-vars
 	constructor(public className: string) {
 		this.setDomElem();
-		this.setActionInput();
+		this.setAction();
+		this.setActionSelect();
+
+		this.defoultText = 'Сколько гостей';
+	}
+
+
+	declOfNum(number: number, words: string[]) {
+		return words[(number % 100 > 4 &&
+			number % 100 < 20) ? 2 :
+			[2, 0, 1, 1, 1, 2][(number % 10 < 5) ?
+				number % 10 : 5]];
+	}
+
+	setInput() {
+
+		let masGuest: string[] = ['гость', 'гостя', 'гостей'];
+		let masBabies: string[] = ['младенец', 'младенца', 'младенцев'];
+
+		let numGuest = Number(this.adultsEl.innerText) +
+			Number(this.childrenEl.innerText);
+		let numBabies = Number(this.babiesEl.innerText);
+
+		let text = '';
+
+		if (numGuest) {
+			text += numGuest + ' ' + this.declOfNum(numGuest, masGuest);
+		}
+
+		if (numBabies) {
+			let comma = text ? ', ' : '';
+			text += comma + numBabies + ' ' +
+				this.declOfNum(numBabies, masBabies);
+		}
+
+		if (text) {
+			this.$clearBut.css('visibility', 'unset');
+			this.inputEl.value = text;
+		} else {
+			this.$clearBut.css('visibility', 'hidden');
+			this.inputEl.value = this.defoultText;
+		}
 
 	}
+
+
+	resetValue() {
+
+		for (let item of this.items) {
+			let listEl = $(item)[0].children;
+			listEl[2].innerText = '0';
+		}
+
+		this.inputEl.value = this.defoultText;
+
+		this.$clearBut.css('visibility', 'hidden');
+	}
+
 
 	setDomElem() {
 		const dropClass = '.' + this.className;
 		const inputClass = dropClass + '__input';
-		const imgClass = dropClass + '__tip';
+		const itemsClass = dropClass + "__select-item";
 		const selectClass = dropClass + '__select';
+		const valueClass = dropClass + '__value';
+		this.imgClass = dropClass + '__tip';
+		this.applyClass = dropClass + "__button-apply";
 
-		const elem = document.querySelector('.' + this.className);
 
-		this.adults = elem.querySelector('[data-type="adults"]');
-		this.children = elem.querySelector('[data-type="children"]');
-		this.babies = elem.querySelector('[data-type="babies"]');
+		this.$clearBut = $(dropClass + "__button-clear");
 
-		this.inputEl = elem.querySelector(inputClass);
-		this.imgEl = elem.querySelector(imgClass);
-		this.selectEl = elem.querySelector(selectClass);
+		this.elem = document.querySelector(dropClass);
+
+		const adults = this.elem.querySelector('[data-type="adults"]');
+		const children = this.elem.querySelector('[data-type="children"]');
+		const babies = this.elem.querySelector('[data-type="babies"]');
+		this.adultsEl = adults.querySelector(valueClass);
+		this.childrenEl = children.querySelector(valueClass);
+		this.babiesEl = babies.querySelector(valueClass);
+
+		this.inputEl = this.elem.querySelector(inputClass);
+		this.selectEl = this.elem.querySelector(selectClass);
+		this.items = this.selectEl.querySelectorAll(itemsClass);
 	}
 
 
 
-	setActionInput() {
-		let flag = false;
+	setActionSelect() {
+
+		const funAct = (e: any) => {
+
+			let listEl = $(e.currentTarget)[0].children;
+
+			let minusEl = listEl[1];
+			let valueEl = listEl[2];
+			let plusEl = listEl[3];
+
+
+			let num = Number(valueEl.innerText);
+
+			if (e.target == minusEl && num) {
+				--num;
+			}
+			else if (e.target == plusEl) {
+				++num;
+			}
+
+			valueEl.innerText = num;
+
+			this.setInput();
+
+		};
+
+		for (let item of this.items) {
+			$(item).on("click", funAct);
+		}
+
+	}
+
+
+	styleWidget(flag = false) {
+
+		let border = '';
+		let color = '';
+
+		if (!$(this.selectEl).is(':visible') && !flag) {
+			border = '0px';
+			color = '0.5)';
+		}
+		else {
+			border = '4px';
+			color = '0.25)';
+		}
+
+		$(this.inputEl).css({
+			"border-bottom-left-radius": border,
+			"border-bottom-right-radius": border,
+			"border": "1px solid rgba(31, 32, 65, " + color
+		});
+	}
+
+
+
+	setAction() {
+
 		let funAct = () => {
-
-			if (!flag) {
-				$(this.inputEl).css({
-					"border-bottom-left-radius": "0px",
-					"border-bottom-right-radius": "0px",
-					"border": "1px solid rgba(31, 32, 65, 0.5)"
-				});
-				flag = true;
-			}
-			else {
-				$(this.inputEl).css({
-					"border-bottom-left-radius": "4px",
-					"border-bottom-right-radius": "4px",
-					"border": "1px solid rgba(31, 32, 65, 0.25)"
-				});
-				flag = false;
-			}
-
+			this.styleWidget();
 			$(this.selectEl).toggle();
 		};
 
 		$(this.inputEl).on("click", funAct);
-		$(this.imgEl).on("click", funAct);
+		$(this.imgClass).on("click", funAct);
+		$(this.applyClass).on("click", funAct);
+		this.$clearBut.on("click", () => this.resetValue());
+
+
+		$(document).on("mouseup", (e: any) => {
+
+			if ($(this.elem).has(e.target).length === 0) {
+
+				this.styleWidget(true);
+				$(this.selectEl).hide();
+			}
+
+		});
+
+
 	}
 
 
@@ -73,13 +197,3 @@ class dropDownGuest {
 
 new dropDownGuest('dropDownGuest');
 
-
-// клик по инпуту - показать селектор или скрыть (изменить стили)
-
-// кнопки minus и plus - увеличивают или уменьшают значение ()
-
-// Кнопка Очистить появляеться лишь когда значения отличны от нуля. 
-
-// Очистить - сброс всех велью, очистка инпута и возврат стандартной надписи "Сколько гостей"
-
-// Применить - формируем список гостей в инпуте - с правильным склонением 
