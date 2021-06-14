7a1e0f9f9e829f63d253b72c1ac346595b666a71
“coder1” <c.o.d.e.r.1@mail.ru>
2021-06-14 20:22:51 +0400

    completed the code and calendar styles (edits needed)

--- src/components/dateDropdown/dateDropDown.js
+++ src/components/dateDropdown/dateDropDown.js
@@ -1,126 +1,169 @@
 
 
-
 class dateDropDown {
 
 	constructor(dateDrop, dateDrop2, datepicker) {
 
 		this.flag = false;
-		this.input1 = $(dateDrop);
-		this.input2 = $(dateDrop2);
-		this.calendar = $(datepicker);
+		this.flTog = false;
+
+		this.input1 = document.querySelector(dateDrop);
+		this.input2 = document.querySelector(dateDrop2);
+		this.calendar = document.querySelector(datepicker);
 
 		this.createCalendar();
-		this.setRange();
+		this.addButtons();
+		this.setActions();
 	}
 
 
 	inputDate(date) {
 		if (this.flag) return;
 
-		this.input1.val('');
-		this.input2.val('');
+		this.input1.value = '';
+		this.input2.value = '';
 
 		let masDate = date.split(",");
 
 		if (masDate.length == 2) {
-			this.input2.val(masDate[1]);
+			this.input2.value = masDate[1];
 		}
 
-		this.input1.val(masDate[0]);
+		this.input1.value = masDate[0];
+	}
+
+	addButtons() {
+
+		const clearB = 'datepicker-buttons__clear';
+		const applyB = 'datepicker-buttons__apply';
+		let elem = this.$calendarObj.$el[0];
+		let datepicker = elem.querySelector('.datepicker');
+
+
+		let divBut = document.createElement('div');
+		divBut.classList.add('datepicker-buttons');
+
+		let spanClr = document.createElement('span');
+		spanClr.classList.add(clearB);
+		//spanClr.style.visibility = 'hidden';
+		spanClr.innerText = 'очистить';
+		divBut.appendChild(spanClr);
+
+		let spanAс = document.createElement('span');
+		spanAс.classList.add(applyB);
+		spanAс.innerText = 'применить';
+		divBut.appendChild(spanAс);
+
+		datepicker.appendChild(divBut);
+
+
+		this.clearButton = spanClr;
+		this.acceptButton = spanAс;
+
 	}
 
 
 	createCalendar() {
-		this.calendarObj = this.calendar.datepicker({
-			range: true,
-			onSelect: (formattedDate) => {
 
+
+		this.$calendarObj = $(this.calendar).datepicker({
+			range: true,
+			prevHtml: '<img src="assets/img/inetrface/arrow_back.svg">',
+			nextHtml: '<img src="assets/img/inetrface/arrow_forward.svg">',
+			navTitles: {
+				days: 'MM yyyy'
+			},
+			onSelect: (formattedDate) => {
 				this.inputDate(formattedDate);
 				this.flag = false;
+				this.clearButton.style.visibility = 'unset';
 			}
 		}).data('datepicker');
-
 	}
+
+
 
 	setRange() {
 
-		this.input1.on('change', () => {
+		this.flag = true;
 
-			this.flag = true;
+		let regexp = /^(0[1-9]|[12][0-9]|3[01])\.(0[1-9]|1[012])\.(19\d\d|20\d\d)$/;
+		let date1 = this.input1.value;
+		let date2 = this.input2.value;
 
-			let dateMas = this.input1.val().split(".");
+		if (!regexp.test(date1)) return;
 
-			let day = dateMas[0];
-			let month = dateMas[1];
-			let year = dateMas[2];
+		function getDate(date) {
+			let mas = date.split(".");
+			const day = mas[0];
+			const month = mas[1];
+			const year = mas[2];
 
-			let date2 = this.input2.val();
-
-			this.calendarObj.clear();
-
-			if (date2 == '') {
-				this.calendarObj.selectDate(
-					new Date(month + '.' + day + '.' + year));
-			} else {
+			return month + '.' + day + '.' + year;
+		}
 
 
-				let masD2 = date2.split(".");
-				let day2 = masD2[0];
-				let month2 = masD2[1];
-				let year2 = masD2[2];
+		this.$calendarObj.clear();
 
-				this.calendarObj.selectDate(
-					[new Date(month + '.' + day + '.' + year),
-					new Date(month2 + '.' + day2 + '.' + year2)]);
-			}
-
-		});
-
-
-
-		this.input2.on('change', () => {
-
-			this.flag = true;
-
-			let dateMas = this.input2.val().split(".");
-
-			let day2 = dateMas[0];
-			let month2 = dateMas[1];
-			let year2 = dateMas[2];
-
-			let date1 = this.input1.val();
-
-			this.calendarObj.clear();
-
-			if (date1) {
-
-				let masD = date1.split(".");
-				let day1 = masD[0];
-				let month1 = masD[1];
-				let year1 = masD[2];
-
-				this.calendarObj.selectDate(
-					[new Date(month1 + '.' + day1 + '.' + year1),
-					new Date(month2 + '.' + day2 + '.' + year2)]);
-			}
-
-		});
+		if (date2 == '') {
+			this.$calendarObj.selectDate(
+				new Date(getDate(date1)));
+		} else {
+			this.$calendarObj.selectDate(
+				[new Date(getDate(date1)),
+				new Date(getDate(date2))]);
+		}
 	}
 
 
+	toggleCal(fl = false) {
 
+		if (this.flTog == fl && this.calendar.style.display == 'flex') {
+			this.calendar.style.display = 'none';
+		}
+		else {
+			this.calendar.style.display = 'flex';
+		}
+
+		this.flTog = fl;
+	}
+
+
+	setActions() {
+		this.input1.addEventListener('change', () => this.setRange());
+		this.input2.addEventListener('change', () => this.setRange());
+
+		this.input1.addEventListener('click', () => this.toggleCal(true));
+		this.input2.addEventListener('click', () => this.toggleCal(false));
+
+
+		this.clearButton.addEventListener('click',
+			() => {
+				this.$calendarObj.clear();
+				this.clearButton.style.visibility = 'hidden';
+			});
+
+
+		this.acceptButton.addEventListener('click',
+			() => {
+				let date1 = this.input1.value;
+				let date2 = this.input2.value;
+
+				if (date1 && date2) {
+					this.calendar.style.display = 'none';
+				}
+				else {
+					alert('Выберите диапазон');
+				}
+			});
+
+
+	}
 }
 
-// '#form-dateDrop' '#form-dateDrop2' '#range-datepicker'
+
 
 
 new dateDropDown('#form-dateDrop', '#form-dateDrop2', '#range-datepicker');
-
-
-
-
-
-
 
 
