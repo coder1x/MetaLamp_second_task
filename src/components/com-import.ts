c92a422448b6cea2f0bb25f9f9700c317d700984
“coder1” <c.o.d.e.r.1@mail.ru>
2021-06-18 21:06:30 +0400

    e-mail validation is done

--- src/components/com-import.ts
+++ src/components/com-import.ts
@@ -4,3 +4,5 @@ import './test/main.ts';
 import './dropDownGuest/dropDownGuest.ts';
 import './maskedTextField/maskedTextField.ts';
 import './dateDropDown/dateDropDown.js';
+import './subscriptionTextField/subscriptionTextField.ts';
+import './validationEmail/validationEmail.ts';
