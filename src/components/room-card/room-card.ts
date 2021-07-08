d6a756e37a2fba569cd06b776114d1f7a133ce7d
“coder1” <c.o.d.e.r.1@mail.ru>
2021-07-08 19:09:54 +0400

    slider in room-card (complite)

---
+++ src/components/room-card/room-card.ts
@@ -0,0 +1,128 @@
+
+class slider {
+
+	className: string;
+	elem: Element;
+	slidesEl: any;
+	dotEl: Element;
+	prevEl: Element;
+	nextEl: Element;
+	indexS: number;
+
+	constructor(className: string, elem: Element) {
+		this.className = className;
+		this.elem = elem;
+		this.indexS = 0;
+		this.startSlider();
+
+	}
+
+
+	getElement(str: string): Element {
+		const selector = this.className + '__' + str + '-wrap';
+		return this.elem.querySelector(selector);
+	}
+
+	startSlider() {
+		this.setDom();
+		this.setVisible(this.indexS, true);
+		this.createDot();
+		this.setAction();
+	}
+
+	setDom() {
+		const slide = this.className + '__slide';
+		this.slidesEl = this.elem.querySelectorAll(slide);
+		this.dotEl = this.getElement('dot');
+		this.prevEl = this.getElement('prev');
+		this.nextEl = this.getElement('next');
+	}
+
+	private getVisible(elem: Element) {
+		let opacity = window.getComputedStyle(elem, null)
+			.getPropertyValue('opacity');
+		return opacity === '0' ? false : true;
+	}
+
+	private toggle(slide: Element) {
+		const fl: boolean = this.getVisible(slide);
+		const prefics = '__slide' + '_visible';
+		const clearName = this.className.replace(/^\./, '') + prefics;
+		let objClass = slide.classList;
+		!fl ? objClass.add(clearName) : objClass.remove(clearName);
+	}
+
+
+	setVisible(index: number, fl = false) {
+		if (!fl) {
+			this.toggle(this.slidesEl[this.indexS]); // удаляем класс с пред идущего слайда
+		}
+
+		this.indexS = index;
+		this.toggle(this.slidesEl[this.indexS]); // показываем новый
+	}
+
+	createDot() {
+
+
+		const classN = this.className.replace(/^\./, '') + '__dot';
+
+		for (let i = 0; i < this.slidesEl.length; i++) {
+			const dot = document.createElement('span');
+			dot.classList.add(classN);
+			dot.setAttribute('data-index', String(i));
+			this.dotEl.appendChild(dot);
+		}
+
+
+
+	}
+
+
+	setAction() {
+
+		this.prevEl.addEventListener('click', () => {
+			if (this.indexS > 0) {
+				this.setVisible(this.indexS - 1);
+			}
+			else {
+				this.setVisible(this.slidesEl.length - 1);
+			}
+		});
+
+		this.nextEl.addEventListener('click', () => {
+			const len = this.slidesEl.length - 1;
+			if (this.indexS < len) {
+				this.setVisible(this.indexS + 1);
+			}
+			else {
+				this.setVisible(0);
+			}
+		});
+
+		this.dotEl.addEventListener('click', (e: any) => {
+
+			const index = Number(e.target.getAttribute('data-index'));
+
+			if (this.indexS != index && !isNaN(index))
+				this.setVisible(index);
+
+		});
+
+	}
+
+
+
+}
+
+function renderSlider(className: string) {
+	let components = document.querySelectorAll(className);
+	let objMas = [];
+	for (let elem of components) {
+		objMas.push(new slider(className, elem));
+	}
+	return objMas;
+}
+
+
+renderSlider('.room-card');
