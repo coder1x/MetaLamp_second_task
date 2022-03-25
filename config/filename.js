459464bf1c277df173b82dd4c893a4a054c37ba4
c.o.d.e.r.1 <c.o.d.e.r.1@mail.ru>
2022-03-25 20:44:14 +0300

    issues #1 - fixed webpack bugs (filename.js)

--- config/filename.js
+++ config/filename.js
@@ -3,6 +3,7 @@
 // в зависемости от типа файла CSS или JS ложим в соответсвующий каталог в деректории dist/assets/ 
 const DP = require('./isDev');
 const PATHS = require('./paths');
+const path = require('path');
 
 module.exports = {
   filename: function (ext) {
@@ -10,12 +11,12 @@ module.exports = {
     let dir = '';
 
     if (ext === 'css') {
-      dir = `${PATHS.assets}css/`;
+      dir = path.join(PATHS.assets, 'css/');
     } else
       if (ext === 'js') {
-        dir = `${PATHS.assets}js/`;
+        dir = path.join(PATHS.assets, 'js/');
       }
-    dir = dir.replace(/\//g, '\\');
+    //  dir = dir.replace(/\//g, '\\');
 
     if (DP.isDev) {
       return `${dir}[name].${ext}`;
